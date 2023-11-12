54574d057d919bab5c5075f3752eacf8c47495fa
mayardes <mayardesoliveira@gmail.com>
2023-11-12 20:50:02 +0000

    feat: add migration folder

--- DevFreela/DevFreela.Infrastructure/Persistence/Migrations/20231112200837_InitialMigration.Designer.cs
+++ DevFreela/DevFreela.Infrastructure/Persistence/Migrations/20231112200837_InitialMigration.Designer.cs
@@ -4,6 +4,7 @@ using DevFreela.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevFreela.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DevFreelaDbContext))]
-    partial class DevFreelaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231112200837_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DevFreela/DevFreela.Infrastructure/Persistence/Migrations/20231112200837_InitialMigration.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevFreela.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_skill",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_skill", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_project",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdClient = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdFreelancer = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TotalCost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FinishAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_project_tb_user_IdClient",
+                        column: x => x.IdClient,
+                        principalTable: "tb_user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_tb_project_tb_user_IdFreelancer",
+                        column: x => x.IdFreelancer,
+                        principalTable: "tb_user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_userSkill",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdUser = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdSkill = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_userSkill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_userSkill_tb_user_IdSkill",
+                        column: x => x.IdSkill,
+                        principalTable: "tb_user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_projectComment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdProject = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdUser = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_projectComment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_projectComment_tb_project_IdProject",
+                        column: x => x.IdProject,
+                        principalTable: "tb_project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_tb_projectComment_tb_user_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "tb_user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_project_IdClient",
+                table: "tb_project",
+                column: "IdClient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_project_IdFreelancer",
+                table: "tb_project",
+                column: "IdFreelancer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_projectComment_IdProject",
+                table: "tb_projectComment",
+                column: "IdProject");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_projectComment_IdUser",
+                table: "tb_projectComment",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_userSkill_IdSkill",
+                table: "tb_userSkill",
+                column: "IdSkill");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_projectComment");
+
+            migrationBuilder.DropTable(
+                name: "tb_skill");
+
+            migrationBuilder.DropTable(
+                name: "tb_userSkill");
+
+            migrationBuilder.DropTable(
+                name: "tb_project");
+
+            migrationBuilder.DropTable(
+                name: "tb_user");
+        }
+    }
+}
