c1768d5ef0ba400901854b330e73531bf43e57b6
mayardes <mayardesoliveira@gmail.com>
2023-11-30 20:55:17 +0000

    fix: add two properties add createdAt - datetime when created updatedAt - datetime when updated

--- DevFreela/DevFreela.Core/Entities/BaseEntity.cs
+++ DevFreela/DevFreela.Core/Entities/BaseEntity.cs
@@ -3,8 +3,7 @@ namespace DevFreela.Core.Entities;
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; }
-    protected BaseEntity()
-    {
-        
-    }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    protected BaseEntity() { }
 }
